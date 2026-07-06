9ec747695d19de0798d3841a74c463f4b1d296fc
jfrancodardengo <jfrancodardengo@gmail.com>
2026-07-06 18:09:47 -0300

    ClientService

---
+++ src/main/java/com/example/devsuperior/client/service/ClientService.java
@@ -0,0 +1,62 @@
+package com.example.devsuperior.client.service;
+
+import com.example.devsuperior.client.dto.ClientResponseDTO;
+import com.example.devsuperior.client.dto.ClientRequestDTO;
+import com.example.devsuperior.client.model.Client;
+import com.example.devsuperior.client.repository.ClientRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    private final ClientRepository repository;
+    private final ModelMapper modelMapper;
+
+    public ClientService(ClientRepository repository, ModelMapper modelMapper) {
+        this.repository = repository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public ClientResponseDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+
+        return modelMapper.map(client, ClientResponseDTO.class);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientResponseDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+
+        return result.map(client -> modelMapper.map(client, ClientResponseDTO.class));
+    }
+
+    @Transactional
+    public ClientResponseDTO insert(ClientRequestDTO dto) {
+        Client client = modelMapper.map(dto, Client.class);
+
+        client = repository.save(client);
+
+        return modelMapper.map(client, ClientResponseDTO.class);
+    }
+
+    @Transactional
+    public ClientResponseDTO update(Long id, ClientRequestDTO dto) {
+        Client client = repository.findById(id).get();
+
+        modelMapper.map(dto, client);
+
+        client = repository.save(client);
+
+        return modelMapper.map(client, ClientResponseDTO.class);
+    }
+
+    @Transactional
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+}
