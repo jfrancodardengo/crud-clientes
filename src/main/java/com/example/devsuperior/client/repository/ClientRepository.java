672ca6b762ae2bbd3bde6bc3e7d404b513a8822f
jfrancodardengo <jfrancodardengo@gmail.com>
2026-07-06 09:51:54 -0300

    model e repository

---
+++ src/main/java/com/example/devsuperior/client/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.example.devsuperior.client.repository;
+
+import com.example.devsuperior.client.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository  extends JpaRepository<Client, Long> {
+}
