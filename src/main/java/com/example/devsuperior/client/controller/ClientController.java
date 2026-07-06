dde4892536c18faa2db5de8f435e0a24bc6be2ea
jfrancodardengo <jfrancodardengo@gmail.com>
2026-07-06 18:32:37 -0300

    ResponseEntity

--- src/main/java/com/example/devsuperior/client/controller/ClientController.java
+++ src/main/java/com/example/devsuperior/client/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.example.devsuperior.client.dto.ClientRequestDTO;
 import com.example.devsuperior.client.service.ClientService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,23 +24,27 @@ public class ClientController {
     }
 
     @GetMapping("/{id}")
-    public ClientResponseDTO findById(@PathVariable Long id) {
-        return clientService.findById(id);
+    public ResponseEntity<ClientResponseDTO> findById(@PathVariable Long id) {
+        ClientResponseDTO dto = clientService.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public Page<ClientResponseDTO> findAll(Pageable pageable) {
-        return clientService.findAll(pageable);
+    public ResponseEntity<Page<ClientResponseDTO>> findAll(Pageable pageable) {
+        Page<ClientResponseDTO> dto = clientService.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
     @PutMapping("/{id}")
-    public ClientResponseDTO update(@PathVariable Long id, @RequestBody ClientRequestDTO dto) {
-        return clientService.update(id, dto);
+    public ResponseEntity<ClientResponseDTO> update(@PathVariable Long id, @RequestBody ClientRequestDTO dto) {
+        ClientResponseDTO response = clientService.update(id, dto);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id){
         clientService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
 
